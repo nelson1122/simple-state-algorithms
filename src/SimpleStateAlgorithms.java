45b91fee10dfcef6dad1541dbb582a3593529aa2
Nelson Quemá <nelson>
2020-05-15 17:01:34 -0500

    adicion funcionalidades solicitadas en el taller

---
+++ src/SimpleStateAlgorithms.java
@@ -0,0 +1,27 @@
+import Tests.TabuSearchTest;
+
+public class Main {
+    public static final Integer UPPER_LIMIT = 100;                     // limite superior
+    public static final Integer LOWER_LIMIT = -100;                    // limite inferior
+    public static final Integer DIMENSIONS[] = {20, 50, 100};          // dimensiones vector solucion
+    public static final Integer OFE_MAX_NUMBER = 5000;                 // evaluaciones funcion objetivo
+    public static final Integer TESTS_NUMBER = 30;                     // nro de pruebas por cada conjunto de parametros
+
+    // parametros lista tabu
+    public static final Integer TABU_LIST_SIZE[] = {50, 100, 150};     // tamaño lista tabu (parametro busqueda tabu)
+    public static final Integer NEIGHBORS_NUMBER[] = {10, 20};         // nro vecinos (parametro busqueda tabu)
+    public static final Double TWEAK_RADIO[] = {0.2d, 0.6d, 1.0d};     // radio tweak
+
+    public static void main(String[] args) {
+        System.out.println("Program has started!");
+
+        TabuSearchTest tabuSearchTest = new TabuSearchTest();
+        tabuSearchTest.runAll();
+//        tabuSearchTest.runSingle();
+
+        System.out.println("Program has finished!");
+    }
+
+}
+
+//        Solution solution = Collections.min(solutionsByTests, Comparator.comparing(s -> s.quality));
